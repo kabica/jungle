5ce491c5e9f9240df766e6f10c8d6319bc4e70e7
Kevin A. Bica <kalex.bica@gmail.com>
2020-03-10 19:47:07 +0000

    add test for user login

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
-RSpec.feature "UserLogins", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  # SETUP
+
+  scenario "They see all products" do
+
+    # create new user
+    @user = User.create! name: 'Alejandro', email: 'kev@gmail.com', password: 'alex123abc', password_confirmation: 'alex123abc'
+    # visit page
+    visit root_path
+
+    # cclick on login (as existing user)
+    click_on 'Login'
+
+    # fill in the email and password fields
+    fill_in 'email', with: 'kev@gmail.com'
+    fill_in 'password', with: 'alex123abc' 
+    
+    # click on submit button to confirm login
+    click_on 'Submit'
+    
+    # confirm that the page displays what user is logged in 
+    expect(page).to have_text 'Signed in as Alejandro |', count: 1
+    
+    save_and_open_screenshot
+ 
+  end
 end
