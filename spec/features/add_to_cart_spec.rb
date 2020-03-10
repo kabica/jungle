2017c008e553d20901e8fb29993ef96f97c00f80
Kevin A. Bica <kalex.bica@gmail.com>
2020-03-10 18:35:35 +0000

    add cart testing

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -4,20 +4,13 @@ RSpec.feature "AddToCart", type: :feature, js: true do
 
   # SETUP
   before :each do
-    @category = Category.create! name: 'Apparel'
-
-    1.times do |n|
-      @category.products.create!(
-      name:  Faker::Hipster.sentence(3),
-      description: Faker::Hipster.paragraph(4),
-      image: open_asset('apparel1.jpg'),
-      quantity: 10,
-      price: 64.99
-      )
-    end
+    
   end
 
   scenario "They see all products" do
+    # create new user
+    @user = User.create! email: 'kev@gmail.com', password: 'alex123cool'
+
     # visit page
     visit root_path
 
