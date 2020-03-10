2ed703c2b0f08a86dc9a4e2c2060afc7e112b76e
Kevin A. Bica <kalex.bica@gmail.com>
2020-03-10 16:00:06 +0000

    refactor migration for users and sales

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+ #get '/' => { controller: 'products', action: 'index' }
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -15,16 +15,24 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
+    resources :sales, only: [:index, :new]
   end
 
+  #resources :about, path: '/about', only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'about' => 'about#show'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
