aef576171486d22149d67bf917e6d51ec9a4bae3
Kevin A. Bica <kalex.bica@gmail.com>
2020-03-10 14:33:01 +0000

    finalize Product spec test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,33 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  describe 'Validate that it:' do
-    # validation tests/examples here
-   it 'is a new instance of Product' do 
-    @product = Product.new
-    expect(@product).to be_a Product
-   end
-
-   it 'has a valid name' do 
-    @category = Catgeory.new
-    @product = Product.new
-    expect(@product.name).to_not be_nil
-   end
-
-   it 'has a valid price' do 
-    # @product = Product.new
-    # expect(@product.price).to_not be_nil
-   end
-
-   it 'has a valid quantity' do
-    # @product = Product.new
-    # expect(@product.quantity).to_not be_nil
-   end
-
-   it 'has a valid parent category' do
-    # @product = Product.new
-    # expect(@product.catgeory).to_not be_nil
-   end
-
+  describe 'Validate Product' do
+    it "is generated with valid attributes" do
+      @category = Category.create(name: "test")
+      @product = Product.create(name: "test", category: @category, quantity: 1, price: 200 )
+      expect(@product).to be_valid
+    end
+    it "is invalid without name" do
+      @category = Category.create(name: "test")
+      @product = Product.create(name: nil, category: @category, quantity: 1, price: 200 )
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank") 
+    end
+    it "is invalid without price" do
+      @category = Category.create(name: "test")
+      @product = Product.create(name: "test", category: @category, quantity: 1, price: nil )
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank") 
+    end
+    it "is invalid without quantity" do
+      @category = Category.create(name: "test")
+      @product = Product.create(name: "test", category: @category, quantity: nil, price: 200 )
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank") 
+    end
+    it "is invalid without category" do
+      @product = Product.create(name: "test", category: nil, quantity: 1, price: 200 )
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank") 
+    end
   end
 end
